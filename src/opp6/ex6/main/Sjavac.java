6cd54cb3226d079d767976d5ac73aff26b98e60a
aviku <avi.kupinsky@mail.huji.ac.il>
2023-01-13 15:01:10 +0200

    sjavac skipMethod

--- src/opp6/ex6/main/Sjavac.java
+++ src/opp6/ex6/main/Sjavac.java
@@ -3,6 +3,7 @@ package opp6.ex6.main;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import static opp6.ex6.main.RegularExpressions.*;
 
 //test
 public class Sjavac {
@@ -13,38 +14,37 @@ public class Sjavac {
             HashMapVariable map = new HashMapVariable();
             globalSearch(args[1], map);
             functionsSearch(args[1], map);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace(); // print 2
         } //TODO: catch InvalidValue / WrongSyntax
     }
 
     private static void globalSearch(String path, HashMapVariable map)
-            throws IOException{ // throws InvalidValue / WrongSyntax
+            throws IOException { // throws InvalidValue / WrongSyntax
         String line;
         try (FileReader fileReader = new FileReader(path);
-             BufferedReader bufferedReader = new BufferedReader(fileReader) ){
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
             while ((line = bufferedReader.readLine()) != null) {
-                line = removeSpacesAndComma(line);
-                if (line.equals("")){ // if line was all comma or an empty line
+                line = line.trim();
+                if (line.equals("") || line.contains("//")) { // if line was all comma or an empty line
                     continue;
                 }
-                if (line.startsWith("void")){
+                if (line.startsWith("void")) {
                     skipMethod(bufferedReader);
+                    continue;
                 }
-                if (HashMapVariable.isLineVariableDeclaration(line)){  // check if its a global declaration
-                    compileVariableDeclaration(line, true,map);
+                if (HashMapVariable.isLineVariableDeclaration(line)) {  // check if its a global declaration
+                    compileVariableDeclaration(line, true, map);
                 }
                 //TODO: check if this line is an assignment (without declaration) and call the function
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     private static void compileVariableDeclaration(String line, boolean global, HashMapVariable map) {
-        map.createVariable(line,global);
+        map.createVariable(line, global);
         //TODO: handle a variable declaration. assumes that the first word is final or a valid type
         //TODO: use HashMapVariable.createVariable function
     }
@@ -52,18 +52,17 @@ public class Sjavac {
     private static void functionsSearch(String path, HashMapVariable map) {  // throws InvalidValue / WrongSyntax
         String line;
         try (FileReader fileReader = new FileReader(path);
-             BufferedReader bufferedReader = new BufferedReader(fileReader) ){
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
             while ((line = bufferedReader.readLine()) != null) {
-                line = removeSpacesAndComma(line);
-                if (line.equals("")){ // if line was all comma or an empty line
+                line = line.trim();
+                if (line.equals("")) { // if line was all comma or an empty line
                     continue;
                 }
-                if (line.startsWith("void")){
+                if (line.startsWith(VOID)) {
                     compileMethod(bufferedReader, map);
                 }
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -80,17 +79,40 @@ public class Sjavac {
         //TODO: compile a while/if statement
     }
 
-    private static void compileAssignment(String line, HashMapVariable map){ // throws InvalidValue / WrongSyntax
+    /**
+     *
+     * @param line
+     * @param map
+     */
+    private static void compileAssignment(String line, HashMapVariable map) { // throws InvalidValue / WrongSyntax
         //TODO: compile assignment, check if variable exist in current scope, use Variable.setValue function
         //TODO: handle an uninitialized global or outer scope variable as a new variable in current scope
     }
 
-    private static void skipMethod(BufferedReader bufferedReader) throws IOException {
-        //TODO: write a function that skips a method - from void....{    to the last return;}
-    }
 
-    private static String removeSpacesAndComma(String line) {
-        //TODO: write a function to remove unnecessary spaces from the beginning of line and commas
-        return line;
+    /**
+     *
+     * @param bufferedReader
+     * @throws IOException
+     */
+    private static void skipMethod(BufferedReader bufferedReader) throws IOException {
+        int countOpenCurlyBrackets = 1;
+        int countCloseCurlyBrackets = 0;
+        String line;
+        while (countOpenCurlyBrackets != countCloseCurlyBrackets) {
+            line = bufferedReader.readLine();
+            if (line.contains("}")) {
+                countCloseCurlyBrackets += 1;
+            } else if (line.startsWith(IF) || line.startsWith(WHILE)) {
+                countOpenCurlyBrackets += 1;
+            }
+            //TODO: write a function that skips a method - from void....{    to the last return;}
+        }
+
+//    private static String removeSpacesAndComma(String line) {
+//        //TODO: write a function to remove unnecessary spaces from the beginning of line and commas
+//        line = line.trim()
+//        return line.trim();
+//    }
     }
 }
